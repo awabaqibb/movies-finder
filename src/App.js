f5e1baa9146ab29ad57bec0f5feada2a56483c18
awabaqibb <awabaqibb@gmail.com>
2022-07-24 22:58:24 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import Movie from "./components/Movie";
+import MovieList from "./components/MovieList";
+import Search from "./components/Search";
+import React, { useEffect, useState } from "react";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?api_key=0dd1f6130aeb52d239896d571a9aa9aa&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=0dd1f6130aeb52d239896d571a9aa9aa&language=en-US&page=1&include_adult=false&query=";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  async function getMovies(API) {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      setMovieList(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
-    </div>
+      <div className="movie-container">
+        {movieList.map((movie) => {
+          return <Movie key={movie.id} {...movie} />;
+        })}
+      </div>
+    </>
   );
 }
 
